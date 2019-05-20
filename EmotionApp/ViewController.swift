83acad394bd0db2ee897a0b01943f2fe5859f82f
Gilang Sinawang <giirugiru@gmail.com>
2019-05-20 13:07:24 +0700

    Bubble floating animation Done Tapo to remove bubble rusak au kenapa :/

--- EmotionApp/ViewController.swift
+++ EmotionApp/ViewController.swift
@@ -35,12 +35,29 @@ class ViewController: UIViewController {
          //MANGGIL IMAGE FILE DARI ASSETS //RANDOMIZE UKURAN BUBBLE NYA
         let randomSize = CGFloat.random(in: 30...100)
         let imageView = UIImageView(frame: CGRect(x: .random(in: 25...389), y: .random(in: 25...871), width: randomSize, height: randomSize))
+        imageView.image = UIImage(named: "blueBubble")
+        UIView.transition(with: self.view, duration: 0.5, options: [.transitionCrossDissolve], animations: {
+            self.view.addSubview(imageView)
+        }, completion: nil)
+//        self.view.addSubview(imageView) //SPAWN BUBBLE NYA
+
+        
         let singleTap = UITapGestureRecognizer(target: self, action: #selector(tapDetected))
         imageView.isUserInteractionEnabled = true
-        
-        imageView.image = UIImage(named: "blueBubble")
         imageView.addGestureRecognizer(singleTap)
-        self.view.addSubview(imageView) //SPAWN BUBBLE NYA
+        
+        
+        //ANIMASI FLOATING BUBBLE
+        
+       let bubblePath = UIBezierPath()
+        bubblePath.move(to: CGPoint(x: .random(in: 25...389), y: 871))
+        bubblePath.addCurve(to: CGPoint(x: .random(in: 25...389), y: .random(in: 25...871)), controlPoint1: CGPoint(x: .random(in: 25...389), y: .random(in: 25...871)), controlPoint2: CGPoint(x: .random(in: 25...389), y: .random(in: 25...871)))
+        let anim = CAKeyframeAnimation(keyPath: "position")
+        anim.path = bubblePath.cgPath
+        anim.rotationMode = CAAnimationRotationMode.rotateAuto
+//        anim.repeatCount = Float.infinity
+        anim.duration = 50
+        imageView.layer.add(anim, forKey: "animate position along path")
         
         //        bubbleImageView.isUserInteractionEnabled = true
         
@@ -52,8 +69,17 @@ class ViewController: UIViewController {
     @objc func tapDetected(bubble: UITapGestureRecognizer){
 //        print(bubble)
         print("tap detected")
-        bubble.view?.alpha = 0
+//        bubble.view?.alpha = 0
 //        bubble.view?.removeFromSuperview()
+//        UIView.animate(withDuration: 0.1, delay: 0, options: [], animations: {
+//            bubble.view?.frame = CGRect(x: (bubble.view?.frame.origin.x)! + 10, y: (bubble.view?.frame.origin.y)!, width: (bubble.view?.frame.width)!, height: (bubble.view?.frame.height)!)
+//        }) { (true) in
+//            UIView.animate(withDuration: 0.1, delay: 0, options: [], animations: {
+//                bubble.view?.frame = CGRect(x: (bubble.view?.frame.origin.x)! - 10, y: (bubble.view?.frame.origin.y)!, width: (bubble.view?.frame.width)!, height: (bubble.view?.frame.height)!)
+//            })
+            bubble.view?.alpha = 1
+//
+//        }
     }
     
 //        UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
